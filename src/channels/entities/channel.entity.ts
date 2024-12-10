680801d8ac1ddc0a5d99ed343686b1b4f9946ae5
chris.tomogo <chris.tomogo@gmail.com>
2024-12-09 18:14:43 -0600

    feat: WIP adding channel table

--- src/channels/entities/channel.entity.ts
+++ src/channels/entities/channel.entity.ts
@@ -1,10 +1,50 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Channel {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  hash: string;
+  @Field(() => String, { description: 'Example field (placeholder)' })
+  @PrimaryGeneratedColumn('uuid')
+  id: string
 
   @Field()
+  @Column()
   name: string
+
+  @Field()
+  @Column()
+  slug: string
+
+  @Field()
+  @Column()
+  description: string
+
+  @Field()
+  @Column()
+  creator_id: string
+
+  @Field()
+  @Column()
+  moderator_id: string // this could be a JSON schema, because a channel can be moderate by a group 
+
+  @Field()
+  @Column()
+  rules: JSON
+
+  @Field()
+  @Column()
+  report_threshold: number
+
+  @Field()
+  @Column()
+  post_count: number
+
+  @Field()
+  @Column()
+  tag: JSON
+
+  @Field()
+  @Column()
+  theme: string
 }
