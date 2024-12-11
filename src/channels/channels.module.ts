3d41b573a9f89cc089ef2d984583c0e7c92d0f63
chris.tomogo <chris.tomogo@gmail.com>
2024-12-11 00:50:48 -0600

    feat: WIP channel module

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ChannelsService } from './channels.service';
 import { ChannelsResolver } from './channels.resolver';
 import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Channel } from './entities/channel.entity';
 
 @Module({
   imports:[
+    TypeOrmModule.forFeature([Channel]),
     AuthModule
   ],
   providers: [ChannelsResolver, ChannelsService]
